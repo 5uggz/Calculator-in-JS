a154ad68dfe4dff57999d62e8f9504c8bbcc1955
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-26 08:25:16 -0400

    Create multiply.js

---
+++ multiply.js
@@ -0,0 +1,8 @@
+function add(a, b)
+{
+  return a * b;
+}
+var num1 = parseFloat(prompt("Enter One Number: "));
+var num2 = parseFloat(prompt("Enter Another Number: "));
+const sum = add(num1, num2);
+console.log("The sum of " + num1 + "*" + num2 + "=" + sum);
