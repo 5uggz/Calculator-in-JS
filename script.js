3a87e34d798941f50679405b49ba6f81fccb41bb
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-26 08:19:44 -0400

    Update and rename script.js to add.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function add()
-{
-  return a + b;
-}
-var num1 = prompt("Enter One Number: ");
-var num2 = prompt("Enter Another Number: ");
-const sum = add(num1, num2);
-console.log("The sum of" + num1 + "+" + num2 + "=" + sum);
