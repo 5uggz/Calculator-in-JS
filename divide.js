d964c9e372d09c3dd9d3a6f80813202aaae6ccc3
Branson <104116765+5uggz@users.noreply.github.com>
2023-10-26 08:51:00 -0400

    Create divide.js

---
+++ divide.js
@@ -0,0 +1,15 @@
+function add(a, b)
+{
+  if(num2 = 0)
+  {
+    return 0;
+  }
+  else
+  {
+  return a / b;
+  }
+}
+var num1 = parseFloat(prompt("Enter One Number: "));
+var num2 = parseFloat(prompt("Enter Another Number: "));
+const sum = add(num1, num2);
+console.log("The sum of " + num1 + "/" + num2 + "=" + sum);
